4cd966369ac1be44a44c4be03310fe8d8fca0856
Rokaya <rokaiamagdi@gmail.com>
2022-12-20 12:52:30 +0200

    M1- create index file

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let id: number = 3;
+let company: string = "4-sale";
+let isPublished: boolean = true;
+
+let x: any = "hi";
+x = 4; //type any alow you to use different type
+
+let ids: number[] = [1, 2, 4, 5, 6];
+let arr: string[] = ["hi", "hello", "welcome"];
+
+let arr2: any[] = [1, true, "hi"];
+
+//Tuple
+let person: [number, string, boolean] = [20, "rokaya", true];
